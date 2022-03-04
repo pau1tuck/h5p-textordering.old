c8b1f639093792ec6aec6e2cccd9aa85fe7650d4
Paul Tuck <pau1tuck@outlook.com>
2022-03-04 14:54:36 +0800

    "2022-03-04_14:54_CST"

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,36 +1,78 @@
-/* eslint-disable no-console */
-H5P.TextOrdering = ((Question: any, $: any, ConfirmationDialog: any) => {
+class TextOrdering extends (H5P.EventDispatcher as { new (): any }) {
+  private jQuery;
+  private answered = false;
   /**
-   * Initialize module.
-   *
-   * @class H5P.TextOrdering
-   * @extends H5P.Question
-   * @param {Object} params // Behavior settings
-   * @param {Number} contentId // Content identification
-   * @param {Object} contentData // Object containing task-specific content data
-   *
-   * @returns {Object} TextOrdering instance
+   * Constructor for Simple Multiple Choice
+   * @param {string} question Question text
+   * @param {string} inputType vertical or horizontal
+   * @param {Array} alternatives Array of strings with answers alternatives
+   * @param {number|*} contentId
+   * @param {Object} contentData
    */
-  const TextOrdering = (options: any, contentId: number, contentData: any) => {
-    Question.call(this);
-    // Set default behavior.
-    $ = $(this);
-    const params = Object.assign(
+  constructor(
+    params: any,
+    { question, listItems = [], inputType }: any,
+    contentId = 0,
+    contentData = {},
+  ) {
+    super();
+
+    // The <li> alternatives, so we can easily append feedback to them
+    this.listItems = [];
+
+    // Keep track of the state
+    this.state = listItems.map((alt: string, i: number) => {
+      return {
+        id: i,
+        text: alt,
+      };
+    });
+
+    /**
+     * Attach library to wrapper
+     * @param {jQuery} $wrapper
+     */
+    this.attach = ($wrapper: any) => {
+      const element = document.createElement("div");
+      element.className = "h5p-simple-multiple-choice";
+      const questionElement = document.createElement("div");
+      questionElement.classList.add("h5p-simple-multiple-choice-question");
+      questionElement.classList.add("h5p-subcontent-question");
+      const questionText = this.createQuestion(this.uniqueName);
+      questionElement.appendChild(questionText);
+
+      element.appendChild(questionElement);
+
+      $wrapper.get(0).appendChild(element);
+    };
+    /**
+     * Create html for multiple choice
+     * @return {HTMLElement} html for multiple choice
+     */
+    this.createQuestion = (id: string): HTMLElement => {
+      const questionElement = document.createElement("div");
+      questionElement.id = id;
+      questionElement.innerHTML = question;
+      return questionElement;
+    };
+
+    params = $.extend(
       true,
       {
-        media: {},
-        taskDescription: "Set in adjectives in the following sentence",
+        question: "No question text provided",
+        content: {
+          blanksText: "",
+        },
+        behaviour: {
+          mode: "typing",
+          selectAlternatives: "alternatives",
+        },
       },
-      options,
+      params,
     );
-    console.log(params);
-    console.log($);
-  };
+    this.jQuery = H5P.jQuery;
+    this.contentId = contentId;
+  }
+}
 
-  TextOrdering.prototype = Object.create(Question.prototype);
-  TextOrdering.prototype.constructor = TextOrdering;
-
-  return TextOrdering;
-})(H5P.jQuery, H5P.Question, H5P.ConfirmationDialog);
-
-export default H5P.TextOrdering;
+export default TextOrdering;
---
+++ src/scripts/old/app_3.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+H5P.TextOrdering = (($: any, Question: any) => {
+  /**
+   * Initialize module.
+   *
+   * @class H5P.TextOrdering
+   * @extends H5P.Question
+   * @param {Object} params // Behavior settings
+   * @param {Number} contentId // Content identification
+   * @param {Object} contentData // Object containing task-specific content data
+   *
+   * @returns {Object} TextOrdering instance
+   */
+  const TextOrdering = (
+    options: any,
+    contentId: number,
+    contentData: any,
+  ): any => {
+    Question.call(this, "text-ordering");
+    // Set default behavior.
+    $ = $(this);
+    const params = Object.assign(
+      true,
+      {
+        taskDescription: "Put the following words in the correct order:",
+      },
+      options,
+    );
+    console.log(params);
+    console.log($);
+    // saves the content id
+    this.contentId = contentId;
+    this.contentData = contentData;
+  };
+
+  TextOrdering.prototype = Object.create(Question.prototype);
+  TextOrdering.prototype.constructor = TextOrdering;
+
+  return TextOrdering;
+})(H5P.jQuery, H5P.Question);
+
+export default H5P.TextOrdering;
